1276d0cef7c9e55f97bc11dca817db8f3eca14d9
Chema Torres <chemaaa94@gmail.com>
2022-12-27 19:21:36 -0600

    changes to the left side of the about portafolioo

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -11,6 +11,8 @@ function About() {
       <h5>Get to Know</h5>
       <h2>About me</h2>
 
+      {/* IMAGE OF THE RIGHT */}
+
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
@@ -18,28 +20,25 @@ function About() {
           </div>
         </div>
 
+        {/* INFORMATION OF THE LEFT */}
         <div className="about__content">
           <div className="about__cards">
             <article className="about__card">
               <FaAward className="about__icon" />
               <h5>Experience</h5>
-              <small>2 year working experience</small>
+              <small>2 year working</small>
             </article>
-          </div>
 
-          <div className="about__cards">
             <article className="about__card">
               <FiUsers className="about__icon" />
               <h5>Clients</h5>
               <small>200+ WorldWide</small>
             </article>
-          </div>
 
-          <div className="about__cards">
             <article className="about__card">
               <MdOutlineFolderSpecial className="about__icon" />
               <h5>Experience</h5>
-              <small>80+ complite Projects</small>
+              <small>80+ completed</small>
             </article>
           </div>
           <p>

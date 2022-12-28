63801d651cf9d347165d160be49fd7811b63370a
Chema Torres <chemaaa94@gmail.com>
2022-12-28 12:49:29 -0600

    mapping the data variable so the information flow in the articule tag

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -1,6 +1,36 @@
 import React from "react";
 import "./testimonials.css";
 import AVT1 from "../../assets/assets/avatar1.jpg";
+import AVT2 from "../../assets/assets/avatar2.jpg";
+import AVT3 from "../../assets/assets/avatar3.jpg";
+import AVT4 from "../../assets/assets/avatar4.jpg";
+
+const data = [
+  {
+    avatar: AVT1,
+    name: "Tina Snow",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste possimus nemo animi aperiam itaque similique ullam provident ut voluptatibus voluptate",
+  },
+  {
+    avatar: AVT2,
+    name: "Michel Jordan",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste possimus nemo animi aperiam itaque similique ullam provident ut voluptatibus voluptate",
+  },
+  {
+    avatar: AVT3,
+    name: "Snow Man",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste possimus nemo animi aperiam itaque similique ullam provident ut voluptatibus voluptate",
+  },
+  {
+    avatar: AVT4,
+    name: "Javier Hernandez",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste possimus nemo animi aperiam itaque similique ullam provident ut voluptatibus voluptate",
+  },
+];
 
 function Testimonials() {
   return (
@@ -9,42 +39,17 @@ function Testimonials() {
       <h2>Testimonials</h2>
 
       <div className="container testimonials__container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVT1} alt="avatar 1" />
-          </div>
-          <h5 className="client__name">Ernest Achiver</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, .
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVT1} alt="avatar 1" />
-          </div>
-          <h5 className="client__name">Ernest Achiver</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, .
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVT1} alt="avatar 1" />
-          </div>
-          <h5 className="client__name">Ernest Achiver</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, .
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVT1} alt="avatar 1" />
-          </div>
-          <h5 className="client__name">Ernest Achiver</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, .
-          </small>
-        </article>
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <article key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt="avatar 1" />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </article>
+          );
+        })}
       </div>
     </section>
   );

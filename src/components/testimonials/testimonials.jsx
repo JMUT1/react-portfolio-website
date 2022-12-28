4291910fa1295a29f1a9c26168edea60a362d047
Chema Torres <chemaaa94@gmail.com>
2022-12-28 12:30:53 -0600

    testimonials card hard code it, media query and css

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import "./testimonials.css";
+import AVT1 from "../../assets/assets/avatar1.jpg";
 
 function Testimonials() {
-  return <section id="testimonials">testimonials</section>;
+  return (
+    <section id="testimonials">
+      <h5>Review from client</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVT1} alt="avatar 1" />
+          </div>
+          <h5 className="client__name">Ernest Achiver</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, .
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVT1} alt="avatar 1" />
+          </div>
+          <h5 className="client__name">Ernest Achiver</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, .
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVT1} alt="avatar 1" />
+          </div>
+          <h5 className="client__name">Ernest Achiver</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, .
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVT1} alt="avatar 1" />
+          </div>
+          <h5 className="client__name">Ernest Achiver</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, .
+          </small>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Testimonials;

c364850adac630a875b7de777b8a692450ee7255
Chema Torres <chemaaa94@gmail.com>
2022-12-28 17:01:46 -0600

    footer jsx css and media querys are in place

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,57 @@
 import React from "react";
 import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
+import { FaTwitter } from "react-icons/fa";
 
 function Footer() {
-  return <section id="footer">footer</section>;
+  return (
+    <footer id="footer">
+      <a href="#" className="footer__logo">
+        JMUT
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contacts">Contacts</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="">
+          <FaFacebookF />
+        </a>
+        <a href="">
+          <BsInstagram />
+        </a>
+        <a href="">
+          <FaTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Chema Torres. All rights reserved</small>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;

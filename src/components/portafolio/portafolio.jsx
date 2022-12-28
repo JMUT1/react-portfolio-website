150d0769c05818aca3bbe9e506498dea52c14b27
Chema Torres <chemaaa94@gmail.com>
2022-12-28 11:53:10 -0600

    portafolio is working, use the map method to get the information I place in the variable data

--- src/components/portafolio/portafolio.jsx
+++ src/components/portafolio/portafolio.jsx
@@ -7,6 +7,51 @@ import IMG4 from "../../assets/assets/portfolio4.jpg";
 import IMG5 from "../../assets/assets/portfolio5.png";
 import IMG6 from "../../assets/assets/portfolio6.jpg";
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "demo 1",
+    github: "https://github.com",
+    demo: "https://dribbble.com/Alien__pixels",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "demo 2",
+    github: "https://github.com",
+    demo: "https://dribbble.com/Alien__pixels",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "demo 3",
+    github: "https://github.com",
+    demo: "https://dribbble.com/Alien__pixels",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "demo 4",
+    github: "https://github.com",
+    demo: "https://dribbble.com/Alien__pixels",
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "demo 5",
+    github: "https://github.com",
+    demo: "https://dribbble.com/Alien__pixels",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "demo 6",
+    github: "https://github.com",
+    demo: "https://dribbble.com/Alien__pixels",
+  },
+];
+
 function Portafolio() {
   return (
     <section id="portafolio">
@@ -14,120 +59,29 @@ function Portafolio() {
       <h2> Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn">
-              Gitbub
-            </a>
-            <a
-              href="https://dribbble.com/Alien__pixels"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn">
-              Gitbub
-            </a>
-            <a
-              href="https://dribbble.com/Alien__pixels"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn">
-              Gitbub
-            </a>
-            <a
-              href="https://dribbble.com/Alien__pixels"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn">
-              Gitbub
-            </a>
-            <a
-              href="https://dribbble.com/Alien__pixels"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn">
-              Gitbub
-            </a>
-            <a
-              href="https://dribbble.com/Alien__pixels"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn">
-              Gitbub
-            </a>
-            <a
-              href="https://dribbble.com/Alien__pixels"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt="" />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn">
+                  Gitbub
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );

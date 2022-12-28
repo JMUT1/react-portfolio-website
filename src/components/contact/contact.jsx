5efa4feb210b054194f69e85ecb4fd6efb424e58
Chema Torres <chemaaa94@gmail.com>
2022-12-28 13:40:13 -0600

    css to the contact options

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,8 +1,70 @@
 import React from "react";
 import "./contact.css";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { BsInstagram } from "react-icons/bs";
+import { ImWhatsapp } from "react-icons/im";
 
 function Contact() {
-  return <section id="contact">contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMailOutline className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>josemariaurenatorres@gmail.com</h5>
+            <a href="mailto:josemariaurenatorres@gmail.com">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsInstagram className="contact__option-icon" />
+            <h4>Instagram</h4>
+            <h5>chemaTorres_</h5>
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <ImWhatsapp className="contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>+52 8112550227</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+528112550227"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        {/* END OF THE CONTACT OPTIONS */}
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;

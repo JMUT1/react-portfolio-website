f34aae22f424786a7fbd0dbd0370580eff482877
Chema Torres <chemaaa94@gmail.com>
2022-12-28 10:00:42 -0600

    serivces are done, css and media querys working right

--- src/components/servic/services.jsx
+++ src/components/servic/services.jsx
@@ -1,7 +1,79 @@
 import React from "react";
+import { BsCheck2 } from "react-icons/bs";
+import "./services.css";
 
 function Services() {
-  return <section id="services">services</section>;
+  return (
+    <section id="services">
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+
+      <div className=" container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BsCheck2 className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+            </li>
+            <li>
+              <BsCheck2 className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+            </li>
+            <li>
+              <BsCheck2 className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF THE UX / UI */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BsCheck2 className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+            </li>
+            <li>
+              <BsCheck2 className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+            </li>
+            <li>
+              <BsCheck2 className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF THE WEB DEVELOPMENT */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BsCheck2 className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+            </li>
+            <li>
+              <BsCheck2 className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+            </li>
+            <li>
+              <BsCheck2 className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF THE CONTENT */}
+      </div>
+    </section>
+  );
 }
 
 export default Services;
